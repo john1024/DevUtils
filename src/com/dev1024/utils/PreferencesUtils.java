7a9608a6dee9e0bbac64c5fb4acc460e0c8c5019
Eric <johnliu.lgq@gmail.com>
2014-07-02 22:06:00 +0800

    Add two Methods. 1. putInt() 2. getInt()

--- src/com/dev1024/utils/PreferencesUtils.java
+++ src/com/dev1024/utils/PreferencesUtils.java
@@ -11,23 +11,36 @@ import android.content.SharedPreferences.Editor;
  */
 public class PreferencesUtils {
 
-	public static SharedPreferences getSharedPreferences(Context context,
-			String name) {
-		return context.getSharedPreferences(name, Context.MODE_PRIVATE);
-	}
+    public static SharedPreferences getSharedPreferences(Context context, String name) {
+        return context.getSharedPreferences(name, Context.MODE_PRIVATE);
+    }
 
-	public static void putString(Context context, String name, String key,
-			String value) {
-		SharedPreferences pre = getSharedPreferences(context, name);
-		Editor editor = null;
-		editor = pre.edit();
-		editor.putString(key, value);
-		editor.commit();
-	}
 
-	public static String getString(Context context, String name, String key) {
-		SharedPreferences pre = getSharedPreferences(context, name);
-		return pre.getString(key, "");
-	}
+    public static void putInt(Context context, String name, String key, int value) {
+        SharedPreferences pre = getSharedPreferences(context, name);
+        Editor editor = null;
+        editor = pre.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+
+    public static void putString(Context context, String name, String key, String value) {
+        SharedPreferences pre = getSharedPreferences(context, name);
+        Editor editor = null;
+        editor = pre.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public static String getString(Context context, String name, String key) {
+        SharedPreferences pre = getSharedPreferences(context, name);
+        return pre.getString(key, "");
+    }
+
+    public static int getInt(Context context, String name, String key) {
+        SharedPreferences pre = getSharedPreferences(context, name);
+        return pre.getInt(key, -1);
+    }
 
 }
